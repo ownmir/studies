deb9b5e786b8f9e5c7821a8882ded751ed7bc1c5
ownmir <ownmir@i.ua>
2021-11-01 12:01:54 +0200

    dj30 samplesite tree_classes

---
+++ dj30/dview.py
@@ -0,0 +1,15 @@
+﻿from django.views.generic.dates import ArchiveIndexView
+from models import MainDuty
+
+class DutyIndexView(ArchiveIndexView):
+    model = MainDuty
+    date_field = "date_duty"
+    date_list_period = "year"
+    template_name = "main/index.html"
+    # context_object_name = "duty"
+    allow_empty = True
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['duties'] = MainDuty.objects.all()
+        return context
